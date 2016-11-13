b7b217d2f95efea3824a895fa428f49b7aca0f84
BernzPRO <dragon000414@gmail.com>
2016-11-13 19:14:10 +0700

    SEAMC TEST

--- app/src/main/java/com/example/bernie/seamc_2017_prever/schedulefragement.java
+++ app/src/main/java/com/example/bernie/seamc_2017_prever/schedulefragement.java
@@ -3,6 +3,7 @@ package com.example.bernie.seamc_2017_prever;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +19,14 @@ public class schedulefragement extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        myview = inflater.inflate(R.layout.schedule_layout,container,false);
+        myview = inflater.inflate(R.layout.schedule_layout, container, false);
         return myview;
+
     }
 }
+
+
+
+
+
+
